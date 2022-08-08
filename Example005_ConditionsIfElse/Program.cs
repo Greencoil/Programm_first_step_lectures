0bda949553a9a2b56e83ef7224fcbab3f5905e48
Gleb <gapodgorsky@yandex.ru>
2022-08-08 15:03:02 +0300

    Create name

---
+++ Example005_ConditionsIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "ланочка")
+{
+    Console.WriteLine("Ура, это же ЛАНОЧКА!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}

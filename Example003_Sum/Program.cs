386b80883d83f2bc954c99c7ce54e6c029651ae2
Gleb <gapodgorsky@yandex.ru>
2022-08-08 14:41:49 +0300

    Add random to Ex.3

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,5 +1,6 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿int numberA = new Random().Next(1, 10); //1 2 3 4 ... 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
-Console.WriteLine(Спасибо);

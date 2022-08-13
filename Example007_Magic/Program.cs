6b1de7f882b595d27d9e39637a8f09d0536b8c70
Gleb <gapodgorsky@yandex.ru>
2022-08-13 13:53:27 +0300

    Changes & add Example008

--- Example007_Magic/Program.cs
+++ Example007_Magic/Program.cs
@@ -23,19 +23,19 @@ int count = 0;
 while(count < 10000)
 {
     int what = new Random().Next(0, 3); //[0;3) 0 1 2
-    if (what == 0);
+    if (what == 0)
     {
         x = (x + xa) / 2;
         y = (y + ya) / 2;
      }
 
-    if (what == 1);
+    if (what == 1)
     {
         x = (x + xb) / 2;
         y = (y + yb) / 2;
      } 
 
-     if (what == 2);
+     if (what == 2)
     {
         x = (x + xc) / 2;
         y = (y + yc) / 2;

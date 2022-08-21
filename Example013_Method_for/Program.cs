c71dfe57c7a174b2384290c13bbf8b6fae6990ce
Gleb <gapodgorsky@yandex.ru>
2022-08-21 18:51:00 +0300

    Add Example13

---
+++ Example013_Method_for/Program.cs
@@ -0,0 +1,12 @@
+﻿string Method4(int count, string text)
+{
+    string result = String.Empty;
+    for( int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+
+string res = Method4(10, "1a");
+Console.WriteLine(res);

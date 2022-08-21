9c603fa660b28b20e5f51fac37d4006f91df02d4
Gleb <gapodgorsky@yandex.ru>
2022-08-21 19:52:39 +0300

    Add Example16

---
+++ Example016_min_max_sorting_algorithm/Program.cs
@@ -0,0 +1,37 @@
+﻿// Отсортировать массив меняя местами рабочий с min найденным
+// 1. Найти позицию минимального элемента в неотсортированной части массива
+// 2. Произвести обмен этого значения со значением первой неотсортированной позиции
+// 3. Повторять пока есть не отсортированные элементы
+
+int[] arr = { 1, 5, 4, 5, 2, 6, 7, 1, 1 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length -1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length ; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition= j;
+        }
+        
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array [minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
